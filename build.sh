68cf9c5ba02ad0ebb1401283e7c00099a5f58d40
Chaitanya-vikas <chaitanyavikas52@gmail.com>
2026-03-11 23:59:13 +0530

    Added automated superuser creation

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create the superuser automatically using Render's Environment Variables
+# The '|| true' prevents the script from crashing on future deploys when the user already exists!
+python manage.py createsuperuser --noinput || true
